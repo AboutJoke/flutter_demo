0cc5a579bd18665f2d78f98d8d0d16d411bafbe8
aboutjoke <paraiy450@gmail.com>
2018-12-13 16:31:18 +0800

    widget state management(3)

--- lib/main.dart
+++ lib/main.dart
@@ -160,7 +160,7 @@ class NewRoute extends StatelessWidget {
         routes: {
           "new_page": (context) => NewRoute(),
         },
-      home: ParentWidget(),
+      home: ParentWidgetC(),
 //        home: new Scaffold(
 //          appBar: new AppBar(
 //            title: new Text("is a new route page"),
@@ -278,4 +278,101 @@ class TapBoxB extends StatelessWidget {
   }
 }
 
+///
+/// 混合管理
+///
+class ParentWidgetC extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ParentWidgetC();
+  }
+
+}
+
+class _ParentWidgetC extends State<ParentWidgetC>{
+  bool _active = false;
+
+  void _handleTapBoxChanged(bool newValue){
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container(
+      child: new TapboxC(
+        active: _active,
+        onChanged: _handleTapBoxChanged,
+      ),
+    );
+  }
+
+
+}
+
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  _TapboxCState createState() => new _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  Widget build(BuildContext context) {
+    // 在按下时添加绿色边框，当抬起时，取消高亮  
+    return new GestureDetector(
+      onTapDown: _handleTapDown, // 处理按下事件
+      onTapUp: _handleTapUp, // 处理抬起事件
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: new Container(
+        child: new Center(
+          child: new Text(widget.active ? 'Active' : 'Inactive',
+              style: new TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? new Border.all(
+            color: Colors.teal[700],
+            width: 10.0,
+          )
+              : null,
+        ),
+      ),
+    );
+  }
+}
 
